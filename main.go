07d718ca46fee4d37659ab44c20c6dba08f10b5c
sinabehmanesh <sina.behmanesh1999@gmail.com>
2023-08-06 17:22:59 +0330

    fixed portrait 404 issue

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gorilla/mux"
 	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func homehandler(w http.ResponseWriter, r *http.Request)  {
+func homehandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("index.html")
 	tmpl.Execute(w, "")
 
-	
 }
 
 func sysloghandler(w http.ResponseWriter, r *http.Request) {
@@ -18,26 +18,25 @@ func sysloghandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, "")
 }
 
+func contacthandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./html/contact.html"))
+	tmpl.Execute(w, "")
+}
 func main() {
 
 	//fileserver
 	fs := http.FileServer(http.Dir("html/"))
 	http.Handle("/html/", http.StripPrefix("/html/", fs))
 
-
 	//Routes
 	r := mux.NewRouter()
 	r.HandleFunc("/", homehandler)
 
-
 	// r.HandleFunc("/admin", adminhandler)
 	r.HandleFunc("/syslog", sysloghandler)
-	// r.HandleFunc("/contact", contacthandler)
+	r.HandleFunc("/contact", contacthandler)
 	// r.HandleFunc("/thiswebsite", thiswebsitehandler)
 	http.Handle("/", r)
-
-
-
 
 	http.ListenAndServe(":3000", r)
 }
