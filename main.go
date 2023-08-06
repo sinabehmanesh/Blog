2c72a7ea9911e900bde72e3177f719fc92d8aee5
sinabehmanesh <sina.behmanesh1999@gmail.com>
2023-08-06 18:52:39 +0330

    added contact me form, mysql connection is on its way!

--- main.go
+++ main.go
@@ -7,21 +7,51 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// handlers defined on routes
+
 func homehandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("index.html")
-	tmpl.Execute(w, "")
+	tmpl.Execute(w, nil)
 
 }
 
 func sysloghandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./html/syslog.html"))
-	tmpl.Execute(w, "")
+	tmpl.Execute(w, nil)
 }
 
 func contacthandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./html/contact.html"))
-	tmpl.Execute(w, "")
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+	contactinfo := contact{
+		email:   r.FormValue("email"),
+		subject: r.FormValue("subject"),
+		message: r.FormValue("message"),
+	}
+	tmpl.Execute(w, struct{ Success bool }{true})
+
+	_ = contactinfo.message
 }
+
+func adminhandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./html/contact.html"))
+	tmpl.Execute(w, nil)
+}
+
+func thiswebsitehandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./html/thiswebsite.html"))
+	tmpl.Execute(w, nil)
+}
+
+type contact struct {
+	email   string
+	subject string
+	message string
+}
+
 func main() {
 
 	//fileserver
@@ -32,10 +62,10 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homehandler)
 
-	// r.HandleFunc("/admin", adminhandler)
+	r.HandleFunc("/admin", adminhandler)
 	r.HandleFunc("/syslog", sysloghandler)
 	r.HandleFunc("/contact", contacthandler)
-	// r.HandleFunc("/thiswebsite", thiswebsitehandler)
+	r.HandleFunc("/thiswebsite", thiswebsitehandler)
 	http.Handle("/", r)
 
 	http.ListenAndServe(":3000", r)
