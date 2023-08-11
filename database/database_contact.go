6bec68cab146570f45dc306b112faa519ca32cfb
sinabehmanesh <sina.behmanesh1999@gmail.com>
2023-08-11 21:58:02 +0330

    switched to native query

--- database/database_contact.go
+++ database/database_contact.go
@@ -1,25 +1,28 @@
 package database_contact
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // ///////////////
 // Types
 // ///////////////
-type contact struct {
+type Contact struct {
+	gorm.Model
 	email   string
 	subject string
 	message string
-	date    string
+	DATE    string
 }
+
+type Contacts []Contact
 
 func Insert_message(contactemail string, contactsubject string, contactmessage string) {
 
@@ -28,13 +31,14 @@ func Insert_message(contactemail string, contactsubject string, contactmessage s
 	// printing the time in string format
 	nowdate := dt.Format(time.RFC3339)
 
-	// var record contact
-	// record.email = contactemail
-	// record.subject = contactsubject
-	// record.message = contactmessage
-	// record.date = nowdate
+	var record Contact
+	record.email = contactemail
+	record.subject = contactsubject
+	record.message = contactmessage
+	record.DATE = nowdate
 
-	record := contact{email: "sina@sina.com", subject: "api test", message: "this is sent from api", data: "today!"}
+	fmt.Println(record.email, record.message, record.subject)
+	fmt.Println("view check!")
 	///////////////////////
 	// Database credentials
 	///////////////////////
@@ -49,15 +53,13 @@ func Insert_message(contactemail string, contactsubject string, contactmessage s
 	DB_PORT := os.Getenv("DB_PORT")
 	DB_NAME := "blog"
 
-	dsn := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := sql.Open("mysql", DB_USERNAME+":"+DB_PASSWORD+"@("+DB_HOST+":"+DB_PORT+")/"+DB_NAME+"?parseTime=true")
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = db
+	db.Ping()
 
-	result := db.Create(&record)
+	query := `INSERT INTO contacts (email, subject, message, date) VALUES (` + record.email + `,` + record.subject + "," + record.message + "," + nowdate + ");"
 
-	fmt.Println(result.Statement)
-	fmt.Println(result.RowsAffected)
+	fmt.Println(query)
 }
