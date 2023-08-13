223f133277e9799765c111166930b8800783e339
sinabehmanesh <sina.behmanesh1999@gmail.com>
2023-08-13 16:47:46 +0330

    added authentication and admin panel login

---
+++ admin/admin.go
@@ -0,0 +1,40 @@
+package admin
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+var (
+	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "authentication-status")
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	fmt.Fprintln(w, "james lied about us!")
+}
+
+func Login(w http.ResponseWriter, r *http.Request) string {
+	session, _ := store.Get(r, "authentication-status")
+
+	adminemail := r.FormValue("adminemail")
+	adminpassword := r.FormValue("adminpassword")
+
+	if adminemail == "sina@sina.com" && adminpassword == "sinasinasina" {
+		session.Values["authenticated"] = true
+		session.Save(r, w)
+	} else {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return "Wrong input, Forbidden!"
+	}
+
+}
