76ae3ffced2cb09371cf20c20e62494933b9d5ea
sinabehmanesh <sina.behmanesh1999@gmail.com>
2023-10-10 20:01:54 +0330

    terrafrom provider fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,9 @@ provider "helm" {
         config_path = var.config_path
     }
 }
+
+resource "helm_release" {
+    name = "blog"
+    repositroy = "oci://registry-1.docker.io/sinabehmanesh"
+    chart = "blog-chart
+}
