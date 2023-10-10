9dbdda24fdff9d227363f84a4a7ae45bfde30056
sinabehmanesh <sina.behmanesh1999@gmail.com>
2023-10-10 19:48:59 +0330

    terrafrom provider fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,12 @@
-required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.11.0"
-    }
-    kubernetes {
-      source = "hashicorp/kubernetes"
-      version = "2.23.0"
+terraform {
+    required_providers {
+        helm = {
+        source = "hashicorp/helm"
+        version = "2.11.0"
+        }
+        kubernetes {
+        source = "hashicorp/kubernetes"
+        version = "2.23.0"
+        }
     }
 }
